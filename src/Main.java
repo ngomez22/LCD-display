3ee6d5dfbeb2482564a4c5013f26d31ed9f0ed6f
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-09-23 22:10:02 -0500

    Read user input

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
+import java.util.List;
 
 public class Main {
 	
 	public static void main(String[] args) {
+		try {
+			Reader r = new Reader();
+			List<Display> input = r.getInput();
+			for(Display d : input) {
+				System.out.println(d.toString());
+			}
+		} catch(Exception e) {
+			System.out.println("Invalid input!");
+		}
 		
+		
+		System.out.println("done!");
 	}
-	
 
 }
