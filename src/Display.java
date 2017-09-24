7bfe418df3d3b45ecd49c7799965bb8a115e94b8
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-09-24 00:53:55 -0500

    Documentation

--- src/Display.java
+++ src/Display.java
@@ -3,6 +3,9 @@ import java.util.List;
 
 public class Display {
 	
+	/**
+	 * Characters and Strings used to write the output
+	 */
 	public static final char VERTICAL_BAR = '|';
 	
 	public static final char HORIZONTAL_BAR = '_';
@@ -27,7 +30,7 @@ public class Display {
 	private List<Integer> digits;
 	
 	/**
-	 * Create a new display to print the given number, with the specified size
+	 * Create a new display to print the given number
 	 * @param size
 	 * @param number
 	 */
@@ -54,6 +57,11 @@ public class Display {
 		return digits;
 	}
 	
+	/**
+	 * Build every row needed to print the number, adding every digit's corresponding row
+	 * @return a String made of multiple lines, containing the LCD representation of the
+	 * 		specified number
+	 */
 	public String buildOutput() {
 		StringBuilder output = new StringBuilder();
 		for(int i = 0; i < height; i++) {
@@ -68,135 +76,198 @@ public class Display {
 		return output.toString();
 	}
 	
-	private String getRow(int digit, int row) {
+	/**
+	 * Build the nth row of the given digit
+	 * @param digit
+	 * @param n
+	 * @return the nth row of the given digit
+	 */
+	private String getRow(int digit, int n) {
 		switch(digit) {
 			case 0:
-				return zeroRow(row);
+				return zeroRow(n);
 			case 1:
-				return oneRow(row);
+				return oneRow(n);
 			case 2:
-				return twoRow(row);
+				return twoRow(n);
 			case 3:
-				return threeRow(row);
+				return threeRow(n);
 			case 4:
-				return fourRow(row);
+				return fourRow(n);
 			case 5:
-				return fiveRow(row);
+				return fiveRow(n);
 			case 6:
-				return sixRow(row);
+				return sixRow(n);
 			case 7:
-				return sevenRow(row);
+				return sevenRow(n);
 			case 8:
-				return eightRow(row);
+				return eightRow(n);
 			case 9:
-				return nineRow(row);
+				return nineRow(n);
 			default:
 				return "";
 		}
 	}
 	
-	private String zeroRow(int row) {
-		if(row == 0)
+	/**
+	 * Specifies how to build the nth row of a 0
+	 * @param n
+	 * @return the nth row of a 0
+	 */
+	private String zeroRow(int n) {
+		if(n == 0)
 			return buildRow(false, true, false);
-		else if(row == height - 1)
+		else if(n == height - 1)
 			return buildRow(true, true, true);
 		else
 			return buildRow(true, false, true);
 	}
 	
-	private String oneRow(int row) {
-		if(row == 0)
+	/**
+	 * Specifies how to build the nth row of a 1
+	 * @param n
+	 * @return the nth row of a 1
+	 */
+	private String oneRow(int n) {
+		if(n == 0)
 			return buildRow(false, false, false);
 		else
 			return buildRow(false, false, true);
 	}
 	
-	private String twoRow(int row) {
-		if(row == 0)
+	/**
+	 * Specifies how to build the nth row of a 2
+	 * @param n
+	 * @return the nth row of a 2
+	 */
+	private String twoRow(int n) {
+		if(n == 0)
 			return buildRow(false, true, false);
-		else if(row < (height / 2))
+		else if(n < (height / 2))
 			return buildRow(false, false, true);
-		else if(row == (height / 2))
+		else if(n == (height / 2))
 			return buildRow(false, true, true);
-		else if(row == height - 1)
+		else if(n == height - 1)
 			return buildRow(true, true, false);
 		else
 			return buildRow(true, false, false);
 	}
 	
-	private String threeRow(int row) {
-		if(row == 0)
+	/**
+	 * Specifies how to build the nth row of a 3
+	 * @param n
+	 * @return the nth row of a 3
+	 */
+	private String threeRow(int n) {
+		if(n == 0)
 			return buildRow(false, true, false);
-		else if(row == (height / 2) || row == height - 1)
+		else if(n == (height / 2) || n == height - 1)
 			return buildRow(false, true, true);
 		else
 			return buildRow(false, false, true);
 	}
 	
-	private String fourRow(int row) {
-		if(row == 0)
+	/**
+	 * Specifies how to build the nth row of a 4
+	 * @param n
+	 * @return the nth row of a 4
+	 */
+	private String fourRow(int n) {
+		if(n == 0)
 			return buildRow(false, false, false);
-		else if(row < (height / 2))
+		else if(n < (height / 2))
 			return buildRow(true, false, true);
-		else if(row == (height / 2))
+		else if(n == (height / 2))
 			return buildRow(true, true, true);
 		else
 			return buildRow(false, false, true);
 	}
 	
-	private String fiveRow(int row) {
-		if(row == 0)
+	/**
+	 * Specifies how to build the nth row of a 5
+	 * @param n
+	 * @return the nth row of a 5
+	 */
+	private String fiveRow(int n) {
+		if(n == 0)
 			return buildRow(false, true, false);
-		else if(row < (height / 2))
+		else if(n < (height / 2))
 			return buildRow(true, false, false);
-		else if(row == (height / 2))
+		else if(n == (height / 2))
 			return buildRow(true, true, false);
-		else if(row == height - 1)
+		else if(n == height - 1)
 			return buildRow(false, true, true);
 		else
 			return buildRow(false, false, true);
 	}
 	
-	private String sixRow(int row) {
-		if(row == 0)
+	/**
+	 * Specifies how to build the nth row of a 6
+	 * @param n
+	 * @return the nth row of a 6
+	 */
+	private String sixRow(int n) {
+		if(n == 0)
 			return buildRow(false, true, false);
-		else if(row < (height / 2))
+		else if(n < (height / 2))
 			return buildRow(true, false, false);
-		else if(row == (height / 2))
+		else if(n == (height / 2))
 			return buildRow(true, true, false);
-		else if(row == height - 1)
+		else if(n == height - 1)
 			return buildRow(true, true, true);
 		else
 			return buildRow(true, false, true);
 	}
 	
-	private String sevenRow(int row) {
-		if(row == 0)
+	/**
+	 * Specifies how to build the nth row of a 7
+	 * @param n
+	 * @return the nth row of a 7
+	 */
+	private String sevenRow(int n) {
+		if(n == 0)
 			return buildRow(false, true, false);
 		else
 			return buildRow(false, false, true);
 	}
 	
-	private String eightRow(int row) {
-		if(row == 0)
+	/**
+	 * Specifies how to build the nth row of an 8
+	 * @param n
+	 * @return the nth row of an 8
+	 */
+	private String eightRow(int n) {
+		if(n == 0)
 			return buildRow(false, true, false);
-		else if(row == (height / 2)  || row == height - 1)
+		else if(n == (height / 2)  || n == height - 1)
 			return buildRow(true, true, true);
 		else
 			return buildRow(true, false, true);
 	}
 	
-	private String nineRow(int row) {
-		if(row == 0)
+	/**
+	 * Specifies how to build the nth row of a 9
+	 * @param n
+	 * @return the nth row of a 9
+	 */
+	private String nineRow(int n) {
+		if(n == 0)
 			return buildRow(false, true, false);
-		else if(row < (height / 2))
+		else if(n < (height / 2))
 			return buildRow(true, false, true);
-		else if(row == (height / 2))
+		else if(n == (height / 2))
 			return buildRow(true, true, true);
 		else
 			return buildRow(false, false, true);
 	}
 	
+	/**
+	 * Build a row according to the parameters
+	 * @param leftEdge. Indicates if the row starts with | or with a blank space
+	 * @param fillMiddle. Indicates if the row will have _ between the edges of blank spaces
+	 * @param rightEdge. Indicates if the row will end with | or with a blank space
+	 * @return a row with length equal to width, made of |, _ and blank spaces
+	 */
 	private String buildRow(boolean leftEdge, boolean fillMiddle, boolean rightEdge) {
 		StringBuilder line = new StringBuilder();
 		if(leftEdge)
